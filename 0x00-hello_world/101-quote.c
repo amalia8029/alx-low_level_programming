42f188f9494ad0eafd6a89f724090b023eda7a13
amalia8029 <amalshadraoui@gmail.com>
2023-06-15 15:37:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <unistd.h>
+#include <unists.h>
 /**
-* main - Prints a text
-*
-* Return: Always 1 (Success)
-*/
+ * main - Prints a text
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n", 59);
-return (1);
+	write(2,"and that piece of artis useful\" - dORA korpar,2015-10-19\n",59);
+	return(1)
 }
