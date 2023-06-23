1b10df368bb48611efbe43ec5e59116e809ce4c2
amalia8029 <amalshadraoui@gmail.com>
2023-06-23 09:52:56 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 int _putchar(char c);
+void print_numbers(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
